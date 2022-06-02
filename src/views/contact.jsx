2b5500bb868b709f000b3c6d44e2ddf111d1b4a2
J.Daniel <juandaniel9619@gmail.com>
2022-06-02 16:41:02 -0400

    Added Map component

--- src/views/contact.jsx
+++ src/views/contact.jsx
@@ -1,7 +1,10 @@
+import { MapContact } from "../components/map/map"
+
 export const Contact = function(){
     return(
         <>
-        <h1>CONTACT</h1>
+
+        <MapContact/>
         <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Numquam aut non quae quos voluptas perferendis esse earum consequatur optio. Vel sapiente facere molestiae ad consequatur voluptates officia reiciendis ipsum. Nihil.</p>
         </>
     )
