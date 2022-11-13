2bd92798f79581c355fa0ecf44ee9e660a01f40a
Samir <Samirfdezc@gmail.com>
2022-11-12 21:32:31 -0400

    Mejorando el calendar

--- src/components/eventsPage/calendar/eventCalendar.jsx
+++ src/components/eventsPage/calendar/eventCalendar.jsx
@@ -1,17 +1,15 @@
 import { useState } from 'react';
 import Calendar from 'react-calendar';
-import 'react-calendar/dist/Calendar.css';
-import eventCalendartStyles from './eventCalendar.module.css'
+import './eventCalendar.css';
 
 
 export const EventCalendar = function() {
 
     const [value, onChange] = useState(new Date());
-    
 
     return (
         <>
-            <div className={eventCalendartStyles.calendar}>
+            <div>
                 <Calendar onChange={onChange} value={value} />
             </div>
         </>
